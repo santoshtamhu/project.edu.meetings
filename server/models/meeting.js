ff90cf6060b2fc7206f954ed72ad347098334b9c
santoshtamhu <187sang@gmail.com>
2024-04-16 15:59:15 +0545

    add timestamp

--- server/models/meeting.js
+++ server/models/meeting.js
@@ -2,36 +2,41 @@ const { default: mongoose } = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const MeetingSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const MeetingSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    month: {
+      type: Number,
+      required: true,
+    },
+    day: {
+      type: Number,
+      required: true,
+    },
+    important: {
+      type: Boolean,
+      required: true,
+    },
+    attractive: {
+      type: Boolean,
+      required: true,
+    },
+    image: String,
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-  },
-  month: {
-    type: Number,
-    required: true,
-  },
-  day: {
-    type: Number,
-    required: true,
-  },
-  important: {
-    type: Boolean,
-    required: true,
-  },
-  attractive: {
-    type: Boolean,
-    required: true,
-  },
-  image: String,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Meeting = mongoose.model("Meetings", MeetingSchema);
 
