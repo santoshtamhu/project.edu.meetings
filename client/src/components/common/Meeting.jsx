e3b81a2913763d6c9a5617b6b7d6e312ad6c2b7d
santoshtamhu <187sang@gmail.com>
2024-04-18 22:57:27 +0545

    add router link to single meeting page

--- client/src/components/common/Meeting.jsx
+++ client/src/components/common/Meeting.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Meeting({ meeting }) {
   let { month } = meeting;
@@ -21,17 +22,19 @@ export default function Meeting({ meeting }) {
   month = monthNames[month];
   const shortMonth = month.slice(0, 3);
   return (
-    <div className="rounded-3xl overflow-hidden">
+    <div className="rounded-3xl h-full overflow-hidden ">
       <div className="relative">
-        <img
-          src="https://students.ubc.ca/sites/students.ubc.ca/files/img_blog_20140819_JumpStart_39.jpg"
-          alt=""
-        />
-        <span className="absolute top-4 left-4 font-semibold bg-white rounded-xl px-4 py-2">
+        <Link to={`/meetings/${meeting._id}`}>
+          <img
+            src="https://students.ubc.ca/sites/students.ubc.ca/files/img_blog_20140819_JumpStart_39.jpg"
+            alt=""
+          />
+        </Link>
+        <span className="absolute top-4 left-4 font-semibold bg-white bg-opacity-90 rounded-xl px-3 py-2">
           ${meeting.price}.00
         </span>
       </div>
-      <div className="grid grid-cols-5  gap-5 p-7 bg-white">
+      <div className="grid grid-cols-5  gap-5 p-7 bg-white h-full">
         <div className="col-span-1 flex items-center flex-col">
           <span className="text-[13px] text-customRed uppercase font-semibold">
             {shortMonth}
@@ -39,7 +42,9 @@ export default function Meeting({ meeting }) {
           <span className="text-xl font-semibold">{meeting.day}</span>
         </div>
         <div className="col-span-4 flex flex-col gap-2">
-          <h2 className="text-[18px] font-semibold">{meeting.title}</h2>
+          <Link to={`/meetings/${meeting._id}`}>
+            <h2 className="text-[18px] font-semibold">{meeting.title}</h2>
+          </Link>
           <p className="text-[14px] leading-6">{meeting.description}</p>
         </div>
       </div>
