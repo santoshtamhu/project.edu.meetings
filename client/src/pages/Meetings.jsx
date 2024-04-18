3f8eb6994b2cb7ea3e63a2ad62f7e77f9e317163
santoshtamhu <187sang@gmail.com>
2024-04-18 22:55:30 +0545

    add filter feature

--- client/src/pages/Meetings.jsx
+++ client/src/pages/Meetings.jsx
@@ -6,24 +6,25 @@ export default function Meetings() {
   const [meetings, setMeetings] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [metadata, setMetadata] = useState({});
-  const [hasNextPage, setHasNextPage] = useState(false);
+  const [filter, setFilter] = useState("all");
   let itemsPerPage = 9;
 
   useEffect(() => {
     const fetchMeetings = async () => {
       try {
         const res = await axios.get(
-          `http://localhost:8000/api/meetings?page=${currentPage}&per_page=${itemsPerPage}`
+          `http://localhost:8000/api/meetings?page=${currentPage}&per_page=${itemsPerPage}&filter=${filter}`
         );
         setMeetings(res.data.meetings);
         setMetadata(res.data.metadata);
-        setHasNextPage(res.data.metadata?.nextPage);
       } catch (err) {
         console.log(err);
       }
     };
     fetchMeetings();
-  }, [currentPage, itemsPerPage]);
+  }, [currentPage, itemsPerPage, filter]);
+
+  const hasNextPage = metadata?.nextPage;
 
   // Total Required Pages
   const totalRequiredPages = metadata.totalPages;
@@ -34,40 +35,58 @@ export default function Meetings() {
     pageNumbers.push(i);
   }
 
+  const filters = [
+    { name: "all meetings", value: "all" },
+    { name: "soon", value: "soon" },
+    { name: "important", value: "important" },
+    { name: "attractive", value: "attractive" },
+  ];
+
   return (
     <div>
-      <div className="bg-[url('assets/images/bg-images/heading-bg.jpg')] bg-cover bg-center text-white uppercase pt-56 pb-32 text-center">
+      <div className="bg-[url('/assets/images/bg-images/heading-bg.jpg')] bg-cover bg-center text-white uppercase pt-56 pb-32 text-center">
         <p className="font-semibold mb-4 text-[15px]">
           here are our upcoming meetings
         </p>
         <h1 className="text-4xl font-bold">upcoming meetings</h1>
       </div>
 
-      {/* FILTERS */}
+      {/* FILTERS CONTAINER */}
 
       <div className="mt-28">
-        <div className="flex flex-wrap justify-center gap-2">
-          <button className="btn w-auto ">All Meetings</button>
-          <button className="btn w-auto">Soon</button>
-          <button className="btn w-auto">Important</button>
-          <button className="btn w-auto">Attractive</button>
+        <div className="flex flex-wrap justify-center gap-3">
+          {filters.map((el, index) => {
+            return (
+              <>
+                <button
+                  key={index}
+                  className={`btn w-auto font-semibold hover:text-white hover:bg-customRed transition duration-300 ease-in-out ${
+                    filter !== el.value ? "bg-white text-customRed" : ""
+                  }`}
+                  onClick={() => setFilter(el.value)}
+                >
+                  {el.name}
+                </button>
+              </>
+            );
+          })}
         </div>
 
-        {/* MEETINGS */}
+        {/* MEETINGS CONTAINER*/}
 
-        <div className="mt-16 container md:mdContainer lg:lgContainer xl:xlContainer 2xl:xxlContainer md:columns-2 md:gap-6 lg:columns-3">
+        <div className="mt-16 container sm:smContainer md:mdContainer lg:lgContainer xl:xlContainer 2xl:xxlContainer md:grid md:grid-cols-2 md:gap-7 lg:grid-cols-3">
           {meetings.map((meeting, index) => {
             return (
-              <div key={index} className="mb-6">
+              <div key={index} className="mb-7 md:mb-0">
                 <Meeting meeting={meeting} />
               </div>
             );
           })}
         </div>
 
-        {/* PAGINATION */}
+        {/* PAGINATION CONTAINER*/}
 
-        <div className="flex gap-1 justify-center mt-8">
+        <div className="flex gap-1 justify-center mt-12">
           {/* Previous Page Button */}
           <button
             className={`pagination ${
