cdc4700854bbcc3e9a0097bc7b0449e31bdb07bd
santoshtamhu <187sang@gmail.com>
2024-04-25 17:39:27 +0545

    test api

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const handleError = require("./middlewares/handleError");
 const uploadsRoute = require("./routers/uploads");
 const adminRouter = require("./routers/admin");
+const Meeting = require("./models/meeting");
 require("dotenv").config();
 const port = process.env.API_PORT;
 
@@ -22,6 +23,11 @@ app.use(express.json());
 app.get("/api/test", (req, res) =>
   res.status(200).json({ message: "hello world" })
 );
+
+app.get("/api/testmeetings", (req, res) => {
+  const meeting = Meeting.find({});
+  res.send(meeting);
+});
 
 //Routes
 app.use("/api/meetings", meetingsRoute);
