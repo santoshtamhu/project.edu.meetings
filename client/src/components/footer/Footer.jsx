c166d8d638cdabc33f9936a9a17df12e0b900750
santoshtamhu <187sang@gmail.com>
2024-04-26 00:11:44 +0545

    add admin dashboard link

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { COPYRIGHT } from "../../constants/copyright";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <div className="text-white border-0 border-t mt-24 py-24 border-white border-opacity-15">
       <div className="container sm:smContainer text-center">
+        <Link to={"/admin"}>Admin Dashboard</Link>
         <p>{COPYRIGHT}</p>
       </div>
     </div>
